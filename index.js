d5fe464b301cbfdfa7d576cc039f55b03e574a47
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-09 18:09:11 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+You are a school administrator trying to manage your district's massive database of students. But the backend developer you hired has made a terrible mistake. They have sent you a dataset where all of your female students were organized correctly (from youngest to oldest), but all of the male students have been organized in the reverse order! (from oldest to youngest). 
+
+Luckily, you've noticed something: All female students are represented in the dataset with even id numbers, while the male students are represented with odd id numbers. What if you could just code some logic to solve this problem yourself?  
+
+Given an array of id numbers, writes an algorithm that sorts the odd numbers in ascending order while leaving the even numbers at their original position.
+
+For example:
+[7, 1] becomes: 
+[1, 7]
+
+[5, 8, 6, 3, 4] becomes:
+[3, 8, 6, 5, 4]
+
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0] becomes:
+[1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+*/
+
+// Write your code below: 
